af65adab7897b9efc33794c3fd2a22b851013814
Andrey <ayyyk.ru@gmail.com>
2021-09-08 02:06:40 +0300

    New methon of element mixing
     Changes to be committed:
    	modified:   index.html
    	modified:   main.js

--- main.js
+++ main.js
@@ -1,46 +1,49 @@
-let seqElem = []; //sequence of elements in the field
+//sequence of elements in the field
+let seqElem = ["a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","a14","a15","a16"]; 
 let smallSeq = []; //cells which can be moved in empty cell position
 let noElem; //empty сell
 let stepCnt = 0;
 let counter;
+let newgame = false;
 
 window.onload = function () {
 	counter = document.getElementById("counter")
-	document.getElementById("restart").onclick = window.onload
+	document.getElementById("restart").onclick = mixin
 
-	let elem; // element of array "elements", randomly selected
-	let elements = ["a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","a14","a15","a16"];
-	for (let i=0; i<=15; i++){
-		// select element randomly and place it on the field in order
-		elem = Math.round(Math.random()*(elements.length-1));
-		document.getElementById(elements[elem]).style.order=i+1;	
-		seqElem[i] = elements[elem];
-		elements.splice(elem, 1); // delete element from the "elements" which was placed 
-	};
+	seqElem.forEach(function(item, index){
+		document.getElementById(item).style.order=index+1;
+	});
 
 	noElem = document.getElementById("a16");
 	
-	setOnClick();
+	setOnClick();	
+}
+
+let mixin = function(){
+	window.onload()
+
+	newgame = false;
+	let elem
+	for (let i = 0; i<20000; i++){
+		elem = Math.round(Math.random()*(smallSeq.length-1));
+		let click = document.getElementById(seqElem[smallSeq[elem]]).onclick()
+	}
+
+	newgame = true;
+	stepCnt = 0
+	counter.innerHTML = 'step: 0';
 }
 
 function setOnClick (){
 	let noElemOrd = noElem.style.order;
 
-	if (noElemOrd>4){
-		smallSeq[smallSeq.length] = noElemOrd-5;	
-	};
+	if (noElemOrd>4) smallSeq.push(noElemOrd-5);
 
-	if (noElemOrd<13){
-		smallSeq[smallSeq.length] = +noElemOrd+3;		
-	};
+	if (noElemOrd<13) smallSeq.push(+noElemOrd+3);
 
-	if (![4, 8, 12, 16].includes(+noElemOrd)){
-		smallSeq[smallSeq.length] = noElemOrd;
-	};
+	if (![4, 8, 12, 16].includes(+noElemOrd)) smallSeq.push(noElemOrd);
 
-	if (![1, 5, 9, 13].includes(+noElemOrd)){
-		smallSeq[smallSeq.length] = noElemOrd-2;
-	}; 
+	if (![1, 5, 9, 13].includes(+noElemOrd)) smallSeq.push(noElemOrd-2); 
 
 	changeEnents(changeOnClick)
 }
@@ -56,15 +59,17 @@ function changeOnClick(){
 
 	setOnClick();
 
-	counter.innerHTML = 'step: ' + ++stepCnt;
+	if (newgame) counter.innerHTML = 'step: ' + ++stepCnt;
 
-	if (seqElem.every((elem, index) => (elem.indexOf((+index+1))>=0) ? true : false)){
+	if (newgame&&(seqElem.every((elem, index) => (elem.indexOf((+index+1))>=0) ? true : false))){
+		newgame = false
 		alert("Win!")
+		
 	}
 }
 
 function changeEnents(func){
-	for (i = 0; i<smallSeq.length; i++) {
-		document.getElementById(seqElem[smallSeq[i]]).onclick = func;
-	};
+	smallSeq.forEach(function(item){
+		document.getElementById(seqElem[item]).onclick = func;
+	});
 }
